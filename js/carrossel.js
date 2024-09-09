dd61fb573bf2475283be47b0ac0a60f5d57578f0
Rayreiss <rayanenunes7@hotmail.com>
2024-09-09 15:44:07 -0300

    primeiro commit

---
+++ js/carrossel.js
@@ -0,0 +1,6 @@
+// Inicializa o carrossel usando jQuery ou Bootstrap nativo
+document.addEventListener('DOMContentLoaded', function() {
+    $('.carousel').carousel({
+        interval: 3000 // Tempo de troca entre as imagens em milissegundos
+    });
+});
---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+// Carrega o header e footer dinamicamente
+document.getElementById("header").innerHTML = '<object type="text/html" data="header.html" ></object>';
+document.getElementById("footer").innerHTML = '<object type="text/html" data="footer.html" ></object>';
